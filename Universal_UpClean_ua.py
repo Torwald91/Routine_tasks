7749f4d63df0d6f88c25ca32dd89ddda89048ded
Oleksii <58832697+Torwald91@users.noreply.github.com>
2023-12-30 15:55:18 +0200

    Scripts --version = 1.0

--- Universal_UpClean_ua.py
+++ Universal_UpClean_ua.py
@@ -140,7 +140,7 @@ def clear_logs():
         if 1 <= time_cl <= 4:
             time_units = ['seconds', 'minutes', 'hours', 'days']
             time_value = input(f'\nВведіть кількість {time_units[time_cl-1]} -> ')
-            os.system(f'journalctl --vacuum-time={time_value}{time_units[time_cl-1][0]}')
+            os.system(f'sudo journalctl --vacuum-time={time_value}{time_units[time_cl-1][0]}')
             print ('Доступний простір:\n')
             os.system('journalctl --disk-usage\n')
             time.sleep(2)
